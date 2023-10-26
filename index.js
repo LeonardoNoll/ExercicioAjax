36eb33708bab74383f84e96f69fe3d0228d777ea
Leonardo Noll Cruz <leonoll8@hotmail.com>
2023-10-26 14:22:12 +0000

    exercício de ajax feito

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 
-var carregando = false; // indica se uma requisição Ajax está em andamento
-
 // função para carregar mais imagens
 function carregarImagens() {
-  // if (carregando) {
-  //   return;
-  // }
-  carregando = true;
   var ajax = new XMLHttpRequest();
-  ajax.open("GET", index.html, true);
+
+  ajax.open("GET", "imgs.json", true);
   ajax.onreadystatechange = function() {
     if (ajax.readyState == 4 && ajax.status == 200) {
       let img = document.createElement("img");
-      img.src = imgs.json;
+      let data = JSON.parse(ajax.responseText);
+      let i = Math.floor(Math.random() * 10);
+      img.src = data.animals[i].imagemUrl;
+      img.style.height = 100;
+      img.style.width = 100;
+      // img.style.border = "1px solid black";
+      document.getElementById("images").appendChild(img);
     }
   };
   ajax.send();
 }
 
+carregarImagens();
 // detecta quando o usuário chegou no final da página e carrega mais imagens
-window.onscroll = function(ev) {
+window.onscroll = function() {
   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
     carregarImagens();
   }
